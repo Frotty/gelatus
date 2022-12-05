dd80a65e76b60647f56ec3112ae27ef9a5b1178f
Frotty <Frotty@users.noreply.github.com>
2022-12-05 20:23:27 +0100

    fixes to make it run again

--- src/ts/Services/NetService.ts
+++ src/ts/Services/NetService.ts
@@ -5,7 +5,14 @@ export class NetService {
 
 	connect(): void {
 		console.log("Connecting to server")
-		this.socket = io();
+		this.socket = io("http://localhost:5000", {
+			reconnectionDelay: 1000,
+			reconnection: true,
+			transports: ["websocket"],
+			agent: false,
+			upgrade: false,
+			rejectUnauthorized: false,
+		});
 
 	}
 }
